1c76634d66f5d95c24757cbb015f2a4a750a2e01
Shruti1549 <pandey.shruti0112@gmail.com>
2025-08-11 22:17:42 +0530

    Add files via upload

---
+++ variable.java
@@ -0,0 +1,18 @@
+public class variable {
+    public static void main(String[] args){
+     /*  int a = 2;
+        int _b = 9;
+        int $c = 2;
+        int rollNumber = 70;
+        System.out.println(a);
+        System.out.println(_b);
+        System.out.println($c);
+        System.out.println(rollNumber);
+        */
+        int x = 2;
+        int y = 7;
+        int z = x + y;
+        System.out.println(z);
+    }
+    
+}
