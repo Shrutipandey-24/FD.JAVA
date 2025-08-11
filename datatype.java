3b108764d951f9747aebc572cbab72ee3bd29218
Shruti1549 <pandey.shruti0112@gmail.com>
2025-08-11 22:20:33 +0530

    Add files via upload

---
+++ datatype.java
@@ -0,0 +1,19 @@
+public class datatype {
+    public static void main(String[] args){
+         int age = 25;
+        float pi = 3.14f;
+        double d = 22.56;
+        char grade = 'A';
+        boolean ispass = true;
+        short s = 32000;
+        long l = 123456789L;
+        byte b = 100;
+        System.out.println(age);
+        System.out.println(3.14f);
+        System.out.println(d);
+        System.out.println(true);
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(b);
+    }
+}
