6a86f9b767856dc4b4b00c6b905387039a7876b6
Shruti1549 <pandey.shruti0112@gmail.com>
2025-08-12 09:45:21 +0530

    Add files via upload

---
+++ nonprimitivedatatype.java
@@ -0,0 +1,13 @@
+public class nonprimitivedatatype {
+    public static void main(String[] args){
+        String name = "shruti";
+        int [] arr1 = {2,3,4,5,6};
+        int arr2[] = {1,2,3,4,5};
+        System.out.println(arr1[1]);
+        System.out.println(arr2[4]);
+        
+
+
+        
+    }
+}
