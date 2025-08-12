acb719b022f914410b9e37b400d2f9e3feb009d5
Shruti1549 <pandey.shruti0112@gmail.com>
2025-08-12 09:46:16 +0530

    Add files via upload

---
+++ userinput.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+public class userinput {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        //taking input from user to add the two number
+        // System.out.println("enter a number");  
+        // int a = sc.nextInt();
+        // System.out.println(a);
+        // System.out.println("enter the number");
+        // int b = sc.nextInt();
+        // System.out.println(b);
+        // int c = a+b;
+        // System.out.println(c);
+        System.out.println("enter the float number");
+        float a = sc.nextFloat();
+        System.out.println(a);
+        System.out.println("enter the secong float number");
+        float b = sc.nextFloat();
+        float c = a+b;
+        System.out.println(c);        
+
+
+    }
+    
+}
