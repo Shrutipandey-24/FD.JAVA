90beb01df2ea6ce327bf7a940d5d01809839dbd2
Shruti1549 <pandey.shruti0112@gmail.com>
2026-01-22 20:15:16 +0530

    Add Methods class to demonstrate string operations

---
+++ Methods.java
@@ -0,0 +1,12 @@
+package String;
+import java.util.Arrays;
+
+public class Methods {
+    public static void main(String[] args) {
+        String name = "Shruti Pandey";
+        System.out.println(Arrays.toString(name.toCharArray()));
+        System.out.println(name.toLowerCase());
+        System.out.println(name);
+
+    }
+}
