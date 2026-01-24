521d0f0065e65cf00f6ccfff40b26d08327ab9f4
Shruti1549 <pandey.shruti0112@gmail.com>
2026-01-24 19:50:39 +0530

    Add RowColmMatrix class with search method

---
+++ rowColumnMatrix.java
@@ -0,0 +1,28 @@
+package Array;
+import java.util.Arrays;
+public class RowColmMatrix {
+    public static void main(String[] args){
+        int[][] arr = {
+                {10,20,30,40},
+                {15,25,26,45},
+                {33,34,38,50}
+
+        };
+     //   System.out.println(Arrays.toString(search(arr, 37)));
+    }
+    static int[] arr(int[][] matrix, int target){
+        int r = 0;
+        int c = matrix.length-1;
+        while(r < matrix.length && c >=0){
+            if(matrix[r][c] == target){
+                return new int[]{r,c};
+            }
+            if(matrix[r][c] < target){
+                r++;
+            }else {
+                c--;
+            }
+        }
+        return new int[]{-1,-1};
+    }
+}
