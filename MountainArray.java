0ca01406003b220bd24fc0bd2d11a40fa87f9eb5
Shruti1549 <pandey.shruti0112@gmail.com>
2026-01-26 18:08:48 +0530

    Implement MountainArray class with peak index method

---
+++ MountainArray.java
@@ -0,0 +1,23 @@
+package Array;
+
+public class MountainArray {
+    public static void main(String[] args){
+
+    }
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        while(start <= end){
+            int mid = start + (end -  start) / 2;
+            if(arr[mid] > arr[mid + 1]){
+                //you are in the decesing part of the array
+                //this is may be an answer but look ar left
+                //this is why end != mid - 1
+                end = mid;
+            }else {
+                start = mid + 1;
+            }
+        }
+        return start;
+    }
+}
