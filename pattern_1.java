4cf243b9d827bd8ff7201085241470d537445501
Shruti1549 <pandey.shruti0112@gmail.com>
2026-02-09 23:14:49 +0530

    Add pattern printing functionality in Pattern_1
    Implement a pattern printing program that displays a right-angled triangle of asterisks.

---
+++ pattern_1.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+public class Pattern_1 {
+    public static void main(String [] args){
+        pattern_1(4);
+    }
+    static void pattern_1(int n){
+        for(int row = 1; row<=n; row++){
+            //for every row, run the col
+            for(int col = 1; col<= row; col++){
+                System.out.print("* ");
+            }
+            //when one row is prtinted, we need to add a newline
+            System.out.println();
+        }
+    }
+}
