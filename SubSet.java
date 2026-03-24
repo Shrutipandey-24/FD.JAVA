4b4931a7921b271b4ef2850f8b57ed8af3450cae
Shruti1549 <pandey.shruti0112@gmail.com>
2026-03-24 19:56:04 +0530

    Add SubSet class for generating subsets of an array
    Implement subset generation for unique and duplicate elements.

---
+++ SubSet.java
@@ -0,0 +1,51 @@
+package BubbleSort;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+public class SubSet {
+    public static void main(String[] args){
+        int[] arr = {1,2,3};
+        List<List<Integer>> ans = subset(arr);
+        for(List<Integer> list: ans){
+            System.out.println(list);
+        }
+    }
+    static List<List<Integer>> subset(int[] arr){
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        for(int num : arr){
+            int size = outer.size();
+            for(int i = 0; i< size; i++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(num);
+                outer.add(internal);
+            }
+        }
+        return outer;
+    }
+
+    static List<List<Integer>> subsetDuplicate(int[] arr){
+        Arrays.sort(arr);
+
+        List<List<Integer>> outer = new ArrayList<>();
+        outer.add(new ArrayList<>());
+        int start = 0;
+        int end = 0;
+        for(int i = 0; i< arr.length; i++){
+            start = 0;
+            if(i > 0 && arr[i] == arr[i-1]){
+                start = end+1;
+            }
+            end = outer.size() - 1;
+            int n = outer.size();
+            for(int j = start; j<n; j++){
+                List<Integer> internal = new ArrayList<>(outer.get(i));
+                internal.add(arr[i]);
+                outer.add(internal);
+            }
+        }
+
+
+        return outer;
+    }
+}
